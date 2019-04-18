bb56bdd66ba18f7fa73dbf5e242ca262c483dbf7
insideman02468 <insideman02468@gmail.com>
2019-04-18 20:26:40 +0900

    Realize deployment heroku using Docker

--- gitpush.sh
+++ gitpush.sh
@@ -4,7 +4,7 @@ git config --global user.name "insideman02468"
 git config --global user.email "insideman02468@gmail.com"
 git remote add origin https://github.com/insideman02468/TestHeroku.git
 git status
-git checkout master
+git checkout base
 git add --all
-git commit -m "modified Procfile"
-git push origin master
+git commit -m "Realize deployment heroku using Docker"
+git push origin base
