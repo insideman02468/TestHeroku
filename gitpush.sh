6838a786299763a7e358b90bc7e7e7a628c4bb4d
insideman02468 <insideman02468@gmail.com>
2019-04-18 20:58:06 +0900

    Success to replace djangogirls

--- gitpush.sh
+++ gitpush.sh
@@ -4,7 +4,7 @@ git config --global user.name "insideman02468"
 git config --global user.email "insideman02468@gmail.com"
 git remote add origin https://github.com/insideman02468/TestHeroku.git
 git status
-git checkout master
+git checkout djangogirls
 git add --all
 git commit -m "Success to replace djangogirls"
-git push origin master
+git push origin djangogirls
