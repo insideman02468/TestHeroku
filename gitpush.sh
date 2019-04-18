23569677aa57a53604f4ca6daa022fdef97cc420
insideman02468 <insideman02468@gmail.com>
2019-04-18 19:37:51 +0900

    Trying to deploy by heroku

--- gitpush.sh
+++ gitpush.sh
@@ -4,7 +4,7 @@ git config --global user.name "insideman02468"
 git config --global user.email "insideman02468@gmail.com"
 git remote add origin https://github.com/insideman02468/TestHeroku.git
 git status
-git checkout next
+git checkout test
 git add --all
-git commit -m "Finish docker build and make mysite."
-git push origin next
+git commit -m "Trying to deploy by heroku"
+git push origin test
