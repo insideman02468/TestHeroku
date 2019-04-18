b36c759020cb88079557f456d76663dfd66d3cd3
insideman02468 <insideman02468@gmail.com>
2019-04-18 20:51:19 +0900

    start djangogirls

--- gitpush.sh
+++ gitpush.sh
@@ -4,7 +4,7 @@ git config --global user.name "insideman02468"
 git config --global user.email "insideman02468@gmail.com"
 git remote add origin https://github.com/insideman02468/TestHeroku.git
 git status
-git checkout work
+git checkout djangogirls
 git add --all
-git commit -m "Realize deployment heroku using Docker"
-git push origin work
+git commit -m "start djangogirls"
+git push origin djangogirls
