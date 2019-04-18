b2356e8be57e784f086b2d0a7ecaba4651a3e2e8
insideman02468 <insideman02468@gmail.com>
2019-04-18 21:33:33 +0900

    Merge from djangogirls

--- gitpush.sh
+++ gitpush.sh
@@ -4,7 +4,7 @@ git config --global user.name "insideman02468"
 git config --global user.email "insideman02468@gmail.com"
 git remote add origin https://github.com/insideman02468/TestHeroku.git
 git status
-git checkout djangogirls
+git checkout master
 git add --all
-git commit -m "Success to modify login view"
-git push origin djangogirls
+git commit -m "Merge from djangogirls"
+git push origin master
