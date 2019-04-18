cbfb537af1a242b2d2e20f2e1eaa7d82513e0673
insideman02468 <insideman02468@gmail.com>
2019-04-19 08:27:48 +0900

    Finish Django nyumon(５)

--- gitpush.sh
+++ gitpush.sh
@@ -4,7 +4,7 @@ git config --global user.name "insideman02468"
 git config --global user.email "insideman02468@gmail.com"
 git remote add origin https://github.com/insideman02468/TestHeroku.git
 git status
-git checkout book
+git checkout book2
 git add --all
-git commit -m "Finish Django nyumon(4)"
-git push origin book
+git commit -m "Finish Django nyumon(５)"
+git push origin book2
