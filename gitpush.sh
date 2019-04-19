ddfce576b57f6652496f1cadb1b13fdf2a191ee7
insideman02468 <insideman02468@gmail.com>
2019-04-19 14:35:12 +0900

    Finish Book Manager

--- gitpush.sh
+++ gitpush.sh
@@ -4,7 +4,7 @@ git config --global user.name "insideman02468"
 git config --global user.email "insideman02468@gmail.com"
 git remote add origin https://github.com/insideman02468/TestHeroku.git
 git status
-git checkout book2
+git checkout master
 git add --all
-git commit -m "Change CSS from master"
-git push origin book2
+git commit -m "Finish Book Manager"
+git push origin master
