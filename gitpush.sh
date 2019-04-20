052c3abaa886d5d8db84da379852b6d00604f99a
insideman02468 <insideman02468@gmail.com>
2019-04-20 21:19:07 +0900

    Start RestAPI

--- gitpush.sh
+++ gitpush.sh
@@ -4,7 +4,7 @@ git config --global user.name "insideman02468"
 git config --global user.email "insideman02468@gmail.com"
 git remote add origin https://github.com/insideman02468/TestHeroku.git
 git status
-git checkout master
+git checkout RestAPI
 git add --all
-git commit -m "Finish Book Manager"
-git push origin master
+git commit -m "Start RestAPI"
+git push origin RestAPI
