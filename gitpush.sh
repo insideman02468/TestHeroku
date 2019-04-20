de17ef14df47eee5dc7224661ff485888789201f
insideman02468 <insideman02468@gmail.com>
2019-04-20 21:46:56 +0900

    Merge RestFilter

--- gitpush.sh
+++ gitpush.sh
@@ -4,7 +4,7 @@ git config --global user.name "insideman02468"
 git config --global user.email "insideman02468@gmail.com"
 git remote add origin https://github.com/insideman02468/TestHeroku.git
 git status
-git checkout RestFilter
+git checkout master
 git add --all
-git commit -m "Realize RestFilter in Blog"
-git push origin RestFilter
+git commit -m "Merge RestFilter"
+git push origin master
